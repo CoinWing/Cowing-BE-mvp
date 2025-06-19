f1665c719a4d4c702e9ffe1fe1cfdccc91ba1758
ekwls20 <ekwls20@gmail.com>
2025-06-20 01:28:18 +0900

    Remove(#2) : 파일 삭제(설명 생략)

--- src/main/java/exchange/exchang_BTC/coin/repository/CoinRepository.java
+++
@@ -1,9 +0,0 @@
-package exchange.exchang_BTC.coin.repository;
-
-import exchange.exchang_BTC.coin.entity.CoinEntity;
-import exchange.exchang_BTC.src.user.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoinRepository extends JpaRepository<CoinEntity, Long> {
-
-}
--- src/main/java/exchange/exchang_BTC/src/trade/repository/TradeRepository.java
+++
@@ -1,9 +0,0 @@
-package exchange.exchang_BTC.src.trade.repository;
-
-import exchange.exchang_BTC.src.trade.entity.TradeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TradeRepository extends JpaRepository<TradeEntity, Integer> {
-}
