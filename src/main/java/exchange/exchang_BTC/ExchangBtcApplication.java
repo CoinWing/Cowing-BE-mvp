5f4ceff101d16484f4ae7314ff756084d1c758dc
yuna <nissy930@naver.com>
2025-06-21 18:05:37 +0900

    Chore(#3) : 스케줄링을 위한 @EnableScheduling 추가

--- src/main/java/exchange/exchang_BTC/ExchangBtcApplication.java
+++ src/main/java/exchange/exchang_BTC/ExchangBtcApplication.java
@@ -2,8 +2,10 @@ package exchange.exchang_BTC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class ExchangBtcApplication {
 
 	public static void main(String[] args) {
